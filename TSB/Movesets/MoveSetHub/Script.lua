7790aedb38864d17277057ddcfdfe0d9765c4b52
TheScript101 <sanmela093@gmail.com>
2025-12-21 12:41:31 -0500

    Create Script.lua

---
+++ TSB/Movesets/MoveSetHub/Script.lua
@@ -0,0 +1,33 @@
+-- Load Rayfield UI
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+
+-- Create Window
+local Window = Rayfield:CreateWindow({
+    Name = "TSB Movesets",
+    LoadingTitle = "TSB Movesets",
+    LoadingSubtitle = "Rayfield UI",
+    ConfigurationSaving = {
+        Enabled = false
+    },
+    Discord = {
+        Enabled = false
+    },
+    KeySystem = false
+})
+
+-- Create Saitama Tab with Image Icon
+local SaitamaTab = Window:CreateTab("Saitama", 17761220757)
+
+-- Example Button
+SaitamaTab:CreateButton({
+    Name = "Kars Moveset",
+    Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/OfficialAposty/RBLX-Scripts/refs/heads/main/UltimateLifeForm.lua"))()
+      wait(1)
+        Rayfield:Notify({
+            Title = "Button Pressed",
+            Content = "Executed! (TEMPORARY — dying disables the moveset. Re-execute to enable again.)"
+            Duration = 2
+        })
+    end
+})
