2e8c5ded2d749035865291d6400aeebf1fe85284
TheScript101 <sanmela093@gmail.com>
2026-03-28 21:56:54 -0400

    Update Scriptdidyblud.lua

--- Mmv/Scriptdidyblud.lua
+++ Mmv/Scriptdidyblud.lua
@@ -139,13 +139,14 @@ button.MouseButton1Click:Connect(function()
 
 	local chosenMap = maps[math.random(1, #maps)]
 
+	SendChatMessage("/map " .. chosenMap)
+	task.wait(0.8)
+
 	SendChatMessage("/murderer 360iytt")
 	task.wait(0.8)
 
 	SendChatMessage("/sheriff wrinkledcomp0sure")
-	task.wait(0.8)
-
-	SendChatMessage("/map " .. chosenMap)
+	
 
 	startCooldown()
 end)
