78d425bc84ed49ec584c10a7157cd604e33016e8
TheScript101 <sanmela093@gmail.com>
2026-04-12 15:44:36 -0400

    Update Loadstring.lua

--- Skyward/Loadstring.lua
+++ Skyward/Loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TheScript101/Gren/refs/heads/main/Skyward/SkywarsCombinationas.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheScript101/Gren/refs/heads/main/Skyward/Main/Script_Combination.lua"))()
