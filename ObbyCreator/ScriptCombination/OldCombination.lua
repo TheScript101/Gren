b325c773eabf4f3d29ecb6ece56b4a922a462d3f
TheScript101 <sanmela093@gmail.com>
2026-06-05 13:47:01 -0400

    Create OldCombination.lua

---
+++ ObbyCreator/ScriptCombination/OldCombination.lua
@@ -0,0 +1,5 @@
+-- Obby Creator:Script combination for building model
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheScript101/Gren/refs/heads/main/ModelSpawner/UpdatedModelSpawner.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheScript101/Gren/refs/heads/main/ModelSpawner/BringParts.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheScript101/Gren/refs/heads/main/PartInpector/Inspect.lua"))()
